802cd24a99bc51e1b5e0d4e0ada6a1229d39730e
Geovanna Otoni <geovannaotoni@gmail.com>
2024-01-04 12:01:04 -0300

    feat: function handleSearch

--- src/helpers/pageFunctions.js
+++ src/helpers/pageFunctions.js
@@ -1,4 +1,4 @@
-import { searchCities } from './weatherAPI';
+import { getWeatherByCity, searchCities } from './weatherAPI';
 
 /**
  * Cria um elemento HTML com as informações passadas
@@ -110,12 +110,18 @@ export function createCityElement(cityInfo) {
 /**
  * Lida com o evento de submit do formulário de busca
  */
-export function handleSearch(event) {
+export async function handleSearch(event) {
   event.preventDefault();
   clearChildrenById('cities');
 
   const searchInput = document.getElementById('search-input');
   const searchValue = searchInput.value;
-  searchCities(searchValue);
-  // seu código aqui
+  const cities = await searchCities(searchValue);
+  const weatherPromises = cities.map((city) => getWeatherByCity(city.url));
+  const weatherList = await Promise.all(weatherPromises);
+  weatherList.forEach((weather) => {
+    const cityElement = createCityElement(weather);
+    document.getElementById('cities').appendChild(cityElement);
+  });
+  console.log(weatherList);
 }
