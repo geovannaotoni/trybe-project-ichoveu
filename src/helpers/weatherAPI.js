108e1e702aabf8409e13cf5e54644d05b4ebe571
Geovanna Otoni <geovannaotoni@gmail.com>
2024-01-04 11:01:29 -0300

    feat: function searchCities

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,21 @@
-// Remova os comentários a medida que for implementando as funções
+export const searchCities = async (term) => {
+  try {
+    const token = import.meta.env.VITE_TOKEN;
+    // https://www.weatherapi.com/docs/
+    const url = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${token}&q=${term}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+    if (data.length === 0) {
+      window.alert('Nenhuma cidade encontrada');
+      return [];
+    }
+    return data;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
